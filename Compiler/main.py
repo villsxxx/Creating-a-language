7f76996f26ebac35db585cd6068f41aa97887517
shredder-vrn <shcherbatovr3ds@gmail.com>
2026-03-07 01:25:42 +0300

    update files from lesson #2

--- Compiler/main.py
+++ Compiler/main.py
@@ -1,36 +1,53 @@
-class MathExpressionParser:
-    def __init__(self, expression: str):
-        self.expression = expression
-        self.pos = 0
+import math
+import builtins
+from parser import Parser
 
-    @property
-    def current(self) -> str:
-        if self.pos < len(self.expression):
-            return self.expression[self.pos]
-        else:
-            return '$'
+class MathExpressionParser(Parser):
 
     def NUMBER(self) -> float:
-        temp = ''
+        self.ws()
+        result = ''
         while self.current.isdigit() or self.current == '.':
-            temp += self.current
+            result += self.current
             self.pos += 1
-        return float(temp)
+        if not result:
+            raise Exception(f'Ожидалось число в позиции {self.pos}')
+        return float(result)
+
+    def IDENT(self) -> str:
+        self.ws()
+        result = ''
+        if self.current.isalpha():
+            while self.current.isalnum():
+                result += self.current
+                self.pos += 1
+        self.ws()
+        return result
 
     def group(self) -> float:
-        if self.current == '(':
-            self.pos += 1
-            res = self.add()
-            self.pos += 1
+        if self.is_parse('('):
+            result = self.expression()
+            self.parse(')')
+        elif self.current.isalpha():
+            ident = self.IDENT()
+            self.parse('(')
+            params = self.params()
+            self.parse(')')
+
+            func = globals().get(ident) or getattr(math, ident, None) or getattr(builtins, ident, None)
+            if not callable(func):
+                raise Exception(f'Не найдена функция {ident}')
+            result = func(*params)
         else:
-            res = self.NUMBER()
-        return res
+            result = self.NUMBER()
+        return result
 
     def add(self) -> float:
         res = self.mult()
-        while self.current in ('+' , '-'):
-            op = self.current
-            self.pos += 1
+        while True:
+            op = self.is_parse('+', '-')
+            if op is None:
+                break
             temp = self.mult()
             if op == '+':
                 res = res + temp
@@ -38,11 +55,23 @@ class MathExpressionParser:
                 res = res - temp
         return res
 
+    def expression(self) -> float:
+        return self.add()
+
+    def params(self) -> list[float]:
+        result = []
+        if not self.is_parse(')'):
+            result.append(self.expression())
+            while self.is_parse(','):
+                result.append(self.expression())
+        return result
+
     def mult(self) -> float:
         res = self.group()
-        while self.current in ('*', '/'):
-            op = self.current
-            self.pos += 1
+        while True:
+            op = self.is_parse('*', '/')
+            if op is None:
+                break
             temp = self.group()
             if op == '*':
                 res = res * temp
@@ -52,12 +81,12 @@ class MathExpressionParser:
 
     def result(self):
         res = self.add()
-        if self.pos != len(self.expression):
-            raise Exception(f'лишний символ {self.current} в позиции {self.pos}')
+        if self.pos != len(self.text):
+            raise Exception(f'Лишний символ {self.current} в позиции {self.pos}')
         return res
 
 def main():
-    expression = '3+2'
+    expression = '3+2*2'
     parser = MathExpressionParser(expression)
     res = parser.result()
     print(res)
