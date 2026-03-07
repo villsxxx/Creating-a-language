ec5d658270118c332e7571abef0f594b689be614
shredder-vrn <shcherbatovr3ds@gmail.com>
2026-03-07 08:52:19 +0300

    update main after practice

--- Compiler/main.py
+++ Compiler/main.py
@@ -1,8 +1,13 @@
 import math
 import builtins
+
 from parser import Parser
 
+
 class MathExpressionParser(Parser):
+    def __init__(self, expression):
+        super().__init__(expression)
+        self.vars = {}
 
     def NUMBER(self) -> float:
         self.ws()
@@ -12,6 +17,7 @@ class MathExpressionParser(Parser):
             self.pos += 1
         if not result:
             raise Exception(f'Ожидалось число в позиции {self.pos}')
+        self.ws()
         return float(result)
 
     def IDENT(self) -> str:
@@ -28,19 +34,21 @@ class MathExpressionParser(Parser):
         if self.is_parse('('):
             result = self.expression()
             self.parse(')')
+            return result
         elif self.current.isalpha():
+            pos = self.pos
             ident = self.IDENT()
-            self.parse('(')
-            params = self.params()
-            self.parse(')')
-
-            func = globals().get(ident) or getattr(math, ident, None) or getattr(builtins, ident, None)
-            if not callable(func):
-                raise Exception(f'Не найдена функция {ident}')
-            result = func(*params)
+            if self.is_parse('('):
+                self.pos = pos
+                result = self.call()
+            else:
+                # Это переменная
+                if ident not in self.vars:
+                    raise Exception(f'Не найдена переменная {ident}')
+                result = self.vars[ident]
+            return result
         else:
-            result = self.NUMBER()
-        return result
+            return self.NUMBER()
 
     def add(self) -> float:
         res = self.mult()
@@ -84,6 +92,38 @@ class MathExpressionParser(Parser):
         if self.pos != len(self.text):
             raise Exception(f'Лишний символ {self.current} в позиции {self.pos}')
         return res
+
+    def call(self):
+        ident = self.IDENT()
+        self.parse('(')
+        params = self.params()
+        self.parse(')')
+        func = globals().get(ident) or getattr(math, ident, None) or getattr(builtins, ident, None)
+        if not callable(func):
+            raise Exception(f'Не найдена функция {ident}')
+        return func(*params)
+
+    def stmt(self):
+        pos = self.pos
+        ident = self.IDENT()
+        if self.is_parse('('):
+            self.pos = pos
+            self.call()
+        else:
+            self.parse('=')
+            val = self.expression()
+            self.vars[ident] = val
+
+    def stmts(self) -> None:
+        self.ws()
+        while self.pos < len(self.text) and self.current != '}':
+            self.stmt()
+
+    def program(self):
+        pass
+
+    def input(self):
+        pass
 
 def main():
     expression = '3+2*2'
